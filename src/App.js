4b8ff20893c8d8522ace36674dcf8a9abe6f9a07
ambrojessica <ambrojessica0@gmail.com>
2021-11-12 11:25:41 -0600

    first push

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React from 'react';
+//import
+import {Link, Route} from 'react-router-dom';
+import Home from './Home';
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div className='App'>
+      <nav>
+      <h1>Lambda Eats!</h1>
+      <Link to="/">Home</Link>
+      </nav>
+
+      <Route path="/">
+        <Home />
+      </Route>
+
+    </div>
   );
 };
 export default App;
---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+//beginning of function 
+const Home = () => {
+
+  return (
+    <h1>hehe</h1>
+  )
+
+
+
+}
+
+export default Home;
